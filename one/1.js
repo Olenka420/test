cf3fdddb94ecad50bf74fc2e79bc376079a38912
Olenka <olga.starchenko420@gmail.com>
2020-07-15 17:12:46 +0300

    first commit

---
+++ one/1.js
@@ -0,0 +1,10 @@
+var str = 'hello';
+str=str[0].toUpperCase()+str.substr
+releaseEvents(1);
+alert(str);
+
+var str = 'hello';
+str=str.split('');
+str[0]=str[0].toUpperCase();
+var result =str.join('');
+alert(result);
